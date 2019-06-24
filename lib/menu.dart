87a2b6cc915c27441436fabbefedb4045877b345
maanoel <manoelvitorbrito@gmail.com>
2019-06-24 20:34:41 -0300

    new button menu and starting principle menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -9,10 +9,75 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blueAccent,title: Text("Good Trip")),
+      appBar:
+          AppBar(backgroundColor: Colors.blueAccent, title: Text("Good Trip")),
       backgroundColor: Colors.white,
-      body: Text("111 world"),
+      body: Container(
+          padding: EdgeInsets.only(bottom: 20.0),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 2.0),
+            child: Row(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Icon(
+                      Icons.exit_to_app,
+                      size: 70.0,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 0.5, horizontal: 1.0),
+                    margin: EdgeInsets.all(2.0),
+                    child: Text("Teste"),
+                  )
+                ]),
+                Row(children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Icon(
+                      Icons.exit_to_app,
+                      size: 70.0,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Container(
+                    padding:
+                    EdgeInsets.symmetric(vertical: 0.5, horizontal: 1.0),
+                    margin: EdgeInsets.all(2.0),
+                    child: Text("Teste"),
+                  )
+                ])
+              ],
+            ),
+          )),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.account_box),
+            title: new Text('Meu Perfil'),
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('Sair'))
+        ],
+      ),
     );
   }
 
+  BoxDecoration boxDecorationMenu() {
+    return BoxDecoration(
+      border: Border.all(),
+      color: Colors.white70,
+    );
+  }
 }
